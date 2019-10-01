60140cba0e312e543b0e8f6695ab481b78c36570
Latifa_Simbini <simbinelatifa@gmail.com>
2019-10-01 14:37:14 +0200

    initial commit

--- semi.js
+++ semi.js
@@ -2,6 +2,8 @@ class JamBuddy {
   constructor() {
     this.note1;
     this.note2;
+    this.result;
+
     this.arr = [
       "A",
       "A#",
@@ -17,19 +19,71 @@ class JamBuddy {
       "G#"
     ];
   }
+
   selectNotes() {
     //random selection
     this.note1 = this.arr[Math.floor(Math.random() * this.arr.length)];
     this.note2 = this.arr[Math.floor(Math.random() * this.arr.length)];
     var arr2 = [this.note1, this.note2];
-    document.getElementById("note1").innerHTML = this.note1;
-    document.getElementById("note2").innerHTML = this.note2;
+
+    // console.log(arr2);
+    for (var i = 0; i < arr2.length; i++) {
+      if (arr2[i] == "A#") {
+        arr2.push("bf");
+
+        return arr2;
+      } else if (arr2[i] == "C#") {
+        arr2.push("df");
+      } else if (arr2[i] == "D#") {
+        arr2.push("ef");
+      } else if (arr2[i] == "F#") {
+        arr2.push("gf");
+      } else if (arr2[i] == "G#") {
+        arr2.push("af");
+      }
+    }
+    console.log(arr2);
+    document.getElementById("note1").innerHTML = arr2[0];
+    document.getElementById("note2").innerHTML = arr2[1];
+    for (var j = 0; j < arr2.length; j++) {
+      if (arr2[j].includes("f") || arr2[j].includes("#")) {
+        return arr2[Math.floor(Math.random() * arr2.length)];
+      }
+    }
     return arr2;
+  }
+  revealAnswer() {
+    var array = [
+      "A",
+      "A#",
+      "BF",
+      "B",
+      "C",
+      "C#",
+      "DF",
+      "D",
+      "D#",
+      "EF",
+      "E",
+      "F",
+      "F#",
+      "GF",
+      "G",
+      "G#",
+      "AF"
+    ];
+    document.getElementById("explanation").innerHTML = array;
+
+    selectNotes();
+  }
+  getRandomNotes() {
+    document.getElementById("explanation").style.display = "none";
   }
   check_answer() {
     // Find note1
     var i = 0;
     for (i = 0; i < this.arr.length; i++) if (this.arr[i] == this.note1) break;
+
     // Find note2
     var j;
     for (j = this.arr.length - 1; j >= i + 1; j--)
@@ -44,25 +98,13 @@ class JamBuddy {
       alert("Congratulations You got it right .Well Done!");
     } else {
       a = false;
-      alert("Wrong answer! Try again")
+      alert("Wrong answer! Try again");
     }
+
     return a;
   }
-  reveal_answer(){
-    document.getElementById("explanation").innerHTML = this.arr;
-    for(var i = 0; i < this.arr.length; i++){
-        if(note1 === this.arr){
-            return elem.style.backgroundColor="#ff0000";
-        }
-        else if(note1 === this.arr){
-            return elem.style.backgroundColor="#ff0000";
-        }
-        else{
-            return elem.style=backgroundColor="grey";
-        }
-    }
-  }
- }
- let buddy = new JamBuddy();
- let notes = buddy.selectNotes();
- let answer= reveal_answer();
+}
+
+let buddy = new JamBuddy();
+let note = buddy.selectNotes();
+
